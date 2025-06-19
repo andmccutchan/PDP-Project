be36ac410fb9482c122ac4bfd10909e235668e4a
andmccutchan <andmccutchan2004@gmail.com>
2025-06-19 15:07:44 -0400

    Started making post card

--- src/components/HomePostCard.tsx
+++ src/components/HomePostCard.tsx
@@ -1,7 +1,18 @@
-import { Card } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 
 const HomePostCard = () => {
-  return <Card></Card>;
+  return (
+    <Card>
+      <CardHeader></CardHeader>
+      <CardContent></CardContent>
+      <CardFooter></CardFooter>
+    </Card>
+  );
 };
 
 export default HomePostCard;
